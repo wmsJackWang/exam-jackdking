9471da663b66ea84976a91d90c23f313a080d6e8
wmsJackWang <ncl@1234>
2024-05-05 22:05:48 +0800

    update

--- source/vue/exam-student/src/layout/index.vue
+++ source/vue/exam-student/src/layout/index.vue
@@ -1,67 +1,72 @@
 <template>
   <el-container>
-    <el-header height="61" class="student-header">
-      <div class="head-user">
-        <el-dropdown trigger="click" placement="bottom">
-          <el-badge :is-dot="messageCount!==0" >
-            <el-avatar  class="el-dropdown-avatar" size="medium"  :src="userInfo.imagePath === null ? require('@/assets/avatar.png') : userInfo.imagePath"></el-avatar>
-          </el-badge>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item @click.native="$router.push({path:'/user/index'})">个人中心</el-dropdown-item>
-            <el-dropdown-item @click.native="$router.push({path:'/user/message'})">
-              <el-badge :value="messageCount" v-if="messageCount!==0">
-                <span>消息中心</span>
-              </el-badge>
-              <span  v-if="messageCount===0">消息中心</span>
-            </el-dropdown-item>
-            <el-dropdown-item @click.native="logout" divided>退出</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
+
+    <div class="main">
+      <div ref = "leftMain" class="left_main" :class="{ left_main_show: !openStatus }">
+        <div class="open_close_v2" ref = "leftOpenClose">
+          <div>
+            <i @click="change" v-if="open_close" class="el-icon-s-fold"></i>
+          </div>
+          <!--        <Tree />-->
+          <!--        <ProgressMenu/>-->
+
+          <el-menu
+            :default-active="defaultUrl"
+            :router="true"
+            class="el-menu-vertical-demo bg_color"
+            style="float: right;width: 300px;text-align: left">
+
+            <el-menu-item index="/file/index">
+              <i class="el-icon-s-home"></i>
+              <span slot="title">首页</span>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <i class="el-icon-time"></i>
+              <span slot="title">最近查看</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <i class="el-icon-collection"></i>
+              <span slot="title">我的收藏</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-school"></i>
+              <span slot="title">知识中心</span>
+            </el-menu-item>
+            <el-menu-item index="5">
+              <i class="el-icon-document"></i>
+              <span slot="title">考试中心</span>
+            </el-menu-item>
+            <el-submenu index="6" class="bg_color">
+              <template slot="title" class="bg_color">
+                <i class="el-icon-time"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group class="bg_color">
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2" class="bg_color">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4" class="bg_color">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+          </el-menu>
+
+        </div>
       </div>
-      <el-menu class="el-menu-title" mode="horizontal" :default-active="defaultUrl" :router="true">
-        <el-menu-item index="/index">首页</el-menu-item>
-        <el-menu-item index="/knowledge/list">进步本</el-menu-item>
-        <el-menu-item index="/paper/index">试卷中心</el-menu-item>
-        <el-menu-item index="/record/index">考试记录</el-menu-item>
-        <el-menu-item index="/question/index">错题本</el-menu-item>
-        <el-menu-item index="/workbenches/shareSpace">工作台</el-menu-item>
-        <el-menu-item index="/splitor/index">分割</el-menu-item>
-      </el-menu>
-      <div>
-        <a href="/"><img src="@/assets/logo2.png" height="56"/></a>
+      <el-divider direction="vertical" class="el-divider--vertical"></el-divider>
+      <div class="right_main">
+        <div class="open_close">
+          <i @click="change" v-if="!open_close" class="el-icon-s-unfold"></i>
+          <el-main>
+            <router-view/>
+          </el-main>
+        </div>
       </div>
-    </el-header>
-    <el-main class="student-main">
-      <router-view v-if="defaultUrl == '/knowledge/index'" style="height:600px;width: 100%"/>
-      <router-view v-if="defaultUrl != '/knowledge/index'"/>
-    </el-main>
-<!--    <el-footer height="340" class="student-footer">-->
-<!--      <div class="container">-->
-<!--        <div class="footer-main">-->
-<!--          <h4>友情链接</h4>-->
-<!--          <a href="https://spring.io/" target="_blank" class="footer-main-link">Spring</a>-->
-<!--          <a href="https://cn.vuejs.org/" target="_blank" class="footer-main-link">Vue</a>-->
-<!--          <a href="https://element.eleme.io/" target="_blank" class="footer-main-link">Element</a>-->
-<!--          <a href="https://www.postgresql.org/" target="_blank" class="footer-main-link">Postgresql</a>-->
-<!--          <a href="https://redis.io/" target="_blank" class="footer-main-link">Redis</a>-->
-<!--          <a href="http://undertow.io/" target="_blank" class="footer-main-link">Undertow</a>-->
-<!--          <a href="http://nginx.org/" target="_blank" class="footer-main-link">Nginx</a>-->
-<!--        </div>-->
-<!--        <div class="footer-main">-->
-<!--          <h4>博客</h4>-->
-<!--          <a href="#" target="_blank" class="footer-main-link">在线讨论</a>-->
-<!--          <a href="https://www.google.com/" target="_blank" class="footer-main-link">Google</a>-->
-<!--          <a href="https://www.oschina.net/" target="_blank" class="footer-main-link">Oschina</a>-->
-<!--          <a href="https://www.infoq.cn/" target="_blank" class="footer-main-link">Infoq</a>-->
-<!--          <a href="https://www.iconfont.cn/" target="_blank" class="footer-main-link">Iconfont</a>-->
-<!--        </div>-->
-<!--        <div class="footer-social">-->
-<!--          <p class="footer-social-title">开源社区</p>-->
-<!--          <a href="https://github.com/alvis-u/exam" target="_blank"> <svg-icon icon-class="social-github" style="width: 32px;height: 32px;" /></a>-->
-<!--          <a href="https://gitee.com/alvis-yu/exam" target="_blank"><svg-icon icon-class="gitee" style="width: 32px;height: 32px;" /></a>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </el-footer>-->
+    </div>
   </el-container>
 </template>
 
@@ -85,6 +90,29 @@ export default {
     this.getUserMessageInfo()
     userApi.getCurrentUser().then(re => {
       _this.userInfo = re.response
+    })
+  },
+  mounted () {
+    this.$nextTick(() => {
+      const width = this.$refs.leftMain.clientWidth
+      console.log('leftMain的宽度是：', width)
+      this.$refs.leftOpenClose.style.marginLeft = width - 30 + 'px'
+      console.log('leftOpenClose的宽度是：', this.$refs.leftOpenClose.style.marginLeft)
+    })
+    // 监听鼠标点击事件
+    document.addEventListener('click', (e) => {
+      if (!this.folderShow && !this.fileShow) return // 如果右键菜单不显示，则不处理点击事件
+      let target = e.target
+      while (target && target.parentNode) {
+        if (target.parentNode.class === 'folderContainer') {
+          return
+        }
+        target = target.parentNode
+      }
+      this.folderShow = false
+      this.fileShow = false // 如果点击的是其他区域，则隐藏
+      this.clickFolderId = -1
+      this.clickFileId = -1
     })
   },
   watch: {
